0e12bd2f4420defae6f3e8c19df9660dbfecbb5e
wasdopit <wasdopit@outlook.com>
2026-01-15 20:37:00 +0000

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -1,1 +1,13 @@
-print("Hello, World!")
+local Workspace = Game:GetService("Workspace")
+
+local Players = Game:GetService("Players")
+local Player = Players.LocalPlayer
+local Character = Player:WaitForChild("Character")
+local HumanoidRootPart = Character:WaitForChild("HumanoidRootPart")
+
+local CollectiblesFolder = Workspace:WaitForChild("Collectibles")
+
+for _, Token in ipairs(CollectiblesFolder:GetChildren()) do
+    local Pos = Token.CFrame
+    HumanoidRootPart.CFrame = Vector3.new(Pos)
+end
